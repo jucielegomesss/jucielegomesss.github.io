cd4f957d7f398044aad155e7e4122130779e784e
Juciele <39685601+jucielegomesss@users.noreply.github.com>
2019-06-01 07:42:55 -0300

    Add files via upload

---
+++ jogomotoresparte4/offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1559385448,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bulletcontroller-sheet0.png",
+		"images/bulletcontroller-sheet1.png",
+		"images/bdown-sheet0.png",
+		"images/bdown-sheet1.png",
+		"images/bleft-sheet0.png",
+		"images/shootbutton-sheet0.png",
+		"images/shootbutton-sheet1.png",
+		"images/starshipcontroller-sheet0.png",
+		"images/starshipcontroller-sheet1.png",
+		"images/sensor-sheet0.png",
+		"images/inimigo-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/parede-sheet0.png",
+		"images/sensor_bala-sheet0.png",
+		"images/powerup-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
